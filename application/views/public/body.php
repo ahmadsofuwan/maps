4b3853ff37417ed576a9290a0b688b9e4a56c3c0
ahmadsofuwan <sofuwan62@gmail.com>
2022-08-13 17:35:26 +0700

    update human

--- application/views/public/body.php
+++ application/views/public/body.php
@@ -106,13 +106,7 @@
             });
             var human = L.icon({
                 iconUrl: '<?= base_url('asset/marker/human.svg') ?>',
-                // shadowUrl: 'leaf-shadow.png',
-
                 iconSize: [50, 95], // size of the icon
-                shadowSize: [50, 64], // size of the shadow
-                iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-                shadowAnchor: [4, 62], // the same for the shadow
-                popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
             });
 
 
