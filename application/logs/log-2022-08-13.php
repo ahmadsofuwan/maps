579758845d17b245793135cd1c1a8d09187fe9e4
ahmadsofuwan <sofuwan62@gmail.com>
2022-08-13 13:47:18 +0700

    update header

---
+++ application/logs/log-2022-08-13.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-08-13 08:45:37 --> 404 Page Not Found: Asset/public
+ERROR - 2022-08-13 08:45:37 --> 404 Page Not Found: Asset/public
+ERROR - 2022-08-13 08:45:50 --> 404 Page Not Found: Asset/public
+ERROR - 2022-08-13 08:45:50 --> 404 Page Not Found: Asset/public
+ERROR - 2022-08-13 08:46:02 --> 404 Page Not Found: Asset/public
